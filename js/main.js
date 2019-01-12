098993257771d6d3a728cc5f1706302ae7dab528
Stanislaw Krzyzewski <s.krzyzewski@gmail.com>
2019-01-12 21:54:12 +0000

    [ACCESSIBILITY] Restaurant img alt text added

--- js/main.js
+++ js/main.js
@@ -172,7 +172,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.alt;
+  image.alt = restaurant.name + ', ' + restaurant.neighborhood;
   li.append(image);
 
   const name = document.createElement('h1');
